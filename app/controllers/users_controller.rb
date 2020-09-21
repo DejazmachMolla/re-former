abcb4d74c9a0f4dcfc08b39dba98898a8acd3189
Dejazmach Getachew <jilogical@gmail.com>
2020-09-21 18:12:30 +0300

    Changed the user registration page to use form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def create
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -9,6 +10,8 @@ class UsersController < ApplicationController
     end
   end
   def new
+    @user = User.new
+
   end
 
   private
