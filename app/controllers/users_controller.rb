36a7fa520ef0d28752dd09038c8814e30af286e5
Dejazmach Getachew <jilogical@gmail.com>
2020-09-22 00:33:08 +0300

    Added the user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,30 @@ class UsersController < ApplicationController
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
   end
+
   def new
     @user = User.new
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
   private
